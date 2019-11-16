366b1ffb1341525b40596d7240a08a7532137235
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-11-16 17:53:02 +1100

    Rename some method function arguments for clarity

--- internal/methods/methods.go
+++ internal/methods/methods.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var methodStrings = []string{
+var httpStrings = []string{
 	"GET",
 	"HEAD",
 	"POST",
@@ -20,27 +20,27 @@ var methodStrings = []string{
 
 // MatchAndStrip returns the method name stripped of its HTTP method and a success flag for that operation
 func MatchAndStrip(methodName string) (string, bool) {
-	for _, methodType := range methodStrings {
-		if MatchInsensitive(methodName, methodType) {
-			return StripInsensitive(methodName, methodType), true
+	for _, httpType := range httpStrings {
+		if MatchInsensitive(methodName, httpType) {
+			return StripInsensitive(methodName, httpType), true
 		}
 	}
 	return "", false
 }
 
-// MatchInsensitive returns true if methodName starts with methodType (case insensitive), is at least one character longer, and follows methodType with an uppercase letter (exported method)
-func MatchInsensitive(methodName, methodType string) bool {
+// MatchInsensitive returns true if methodName starts with httpType (case insensitive), is at least one character longer, and follows httpType with an uppercase letter (exported method)
+func MatchInsensitive(methodName, httpType string) bool {
 	nameLength := len(methodName)
-	typeLength := len(methodType)
-	return nameLength > typeLength && // method name is at least one character longer than method type
-		strings.ToLower(methodName[:typeLength]) == strings.ToLower(methodType) && // method name starts with method type
-		strings.ContainsAny(methodName[typeLength:typeLength+1], "ABCDEFGHIJKLMNOPQRSTUVWXYZ") // first character after method type in method name is uppercase alphabetical
+	typeLength := len(httpType)
+	return nameLength > typeLength && // method name is at least one character longer than httpType
+		strings.ToLower(methodName[:typeLength]) == strings.ToLower(httpType) && // method name starts with httpType
+		strings.ContainsAny(methodName[typeLength:typeLength+1], "ABCDEFGHIJKLMNOPQRSTUVWXYZ") // first character after httpType in method name is uppercase alphabetical
 }
 
-// StripInsensitive returns the method name stripped of a prepending method type, panics if that isn't possible
-func StripInsensitive(methodName, methodType string) string {
-	if !MatchInsensitive(methodName, methodType) {
-		panic(fmt.Sprintf("httpgrpc: cannot strip invalid method name/type combination %s/%s", methodName, methodType))
+// StripInsensitive returns the method name stripped of a prepending httpType, panics if that isn't possible
+func StripInsensitive(methodName, httpType string) string {
+	if !MatchInsensitive(methodName, httpType) {
+		panic(fmt.Sprintf("httpgrpc: cannot strip invalid method name/type combination %s/%s", methodName, httpType))
 	}
-	return methodName[len(methodType):]
+	return methodName[len(httpType):]
 }
