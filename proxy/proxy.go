9efcde2f11b76f96de05c71744e26e54ac04a4ec
Luke Kennedy <luke.l.kennedy@gmail.com>
2019-11-17 09:08:39 +1100

    Move function between files

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -2,6 +2,8 @@ package proxy
 
 import (
 	"context"
+	"fmt"
+	"reflect"
 
 	"github.com/LLKennedy/httpgrpc"
 )
@@ -14,4 +16,26 @@ func (s *Server) Proxy(ctx context.Context, req *httpgrpc.Request) (*httpgrpc.Re
 	}
 	_ = innerMethod // use reflection to call this
 	return (&httpgrpc.UnimplementedExposedServiceServer{}).Proxy(ctx, req)
+}
+
+func (s *Server) fetchInnerMethod(methodType httpgrpc.Method, name string) (reflect.Method, error) {
+	// innerName, valid := matchAndStrip(name)
+	// if !valid {
+	// 	return reflect.Method{}, fmt.Errorf("no HTTP method type prepending procedure name")
+	// }
+	// switch methodType {
+	// case httpgrpc.Method_UNKNOWN:
+	// 	return reflect.Method{}, fmt.Errorf("httpgrpc: unknown HTTP method")
+	// case httpgrpc.Method_GET:
+	// case httpgrpc.Method_HEAD:
+	// case httpgrpc.Method_POST:
+	// case httpgrpc.Method_PUT:
+	// case httpgrpc.Method_DELETE:
+	// case httpgrpc.Method_CONNECT:
+	// case httpgrpc.Method_OPTIONS:
+	// case httpgrpc.Method_TRACE:
+	// case httpgrpc.Method_PATCH:
+	// default:
+	return reflect.Method{}, fmt.Errorf("httpgrpc: unknown HTTP method")
+	// }
 }
