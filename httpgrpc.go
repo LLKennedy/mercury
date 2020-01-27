c9ce348e58e17f2488cfdbfb97f0abd7e8cee10e
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-01-27 13:29:56 +1100

    Proxy requests as a client

--- httpgrpc.go
+++ httpgrpc.go
@@ -1,16 +1,18 @@
 package httpgrpc
 
 import (
+	"context"
 	"net/http"
 
-	"github.com/LLKennedy/httpgrpc/converter"
+	"github.com/LLKennedy/httpgrpc/convert"
+	"github.com/LLKennedy/httpgrpc/logs"
 	"github.com/LLKennedy/httpgrpc/proxy"
 	"google.golang.org/grpc"
 )
 
 // ProxyRequest proxies an HTTP request through a GRPC connection compliant with httpgrpc/proto
-func ProxyRequest(w http.ResponseWriter, r *http.Request, conn *grpc.ClientConn) {
-	converter.ProxyRequest(w, r, conn)
+func ProxyRequest(ctx context.Context, w http.ResponseWriter, r *http.Request, procedure string, conn *grpc.ClientConn, txid string, loggers ...logs.Writer) {
+	convert.ProxyRequest(ctx, w, r, procedure, conn, txid, loggers...)
 }
 
 // NewServer creates a new server to convert httpgrpc/proto messages to service-specific messages
