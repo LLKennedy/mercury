f18f1a560c7bfe80a3b9f42c1c14f820becaecfb
Luke Kennedy <luke.l.kennedy@gmail.com>
2020-02-25 14:17:28 +1100

    Rename proto package to ease import conflicts

--- genproto.ps1
+++ genproto.ps1
@@ -3,5 +3,5 @@ $IncludeRule = "*.proto"
 $ExcludeRUle = [Regex]'.*google.*'
 $ProtoFiles = Get-ChildItem -path $Directory -Recurse -Include $IncludeRule | Where-Object FullName -NotMatch $ExcludeRUle
 foreach ($file in $ProtoFiles) {
-    protoc --proto_path="$($file.DirectoryName)" -I "./proto"  --go_out=plugins=grpc:"$($file.DirectoryName)" $file.FullName
+    protoc --proto_path="$($file.DirectoryName)" --go_out=plugins=grpc:"$($file.DirectoryName)" $file.FullName
 }
