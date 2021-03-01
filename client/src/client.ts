c63c2ea3e0067cab3ea0421f258bf4ff314da085
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-01 20:57:20 +1100

    Start to flesh out a websocket UI client

--- client/src/client.ts
+++ client/src/client.ts
@@ -1,16 +1,32 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+import { EstablishedWebsocket, HTTPgRPCWebSocket } from "websocket";
+
+export type Parser<T> = ((res: Object) => T) | ((res: Object) => Promise<T>);
 
 export class Client {
+	private axiosClient: AxiosInstance;
 	private basePath: string;
 	private secure: boolean;
-	constructor(basePath: string = "localhost/api", secure: boolean = true) {
+	constructor(basePath: string = "localhost/api", secure: boolean = true, client: AxiosInstance = axios) {
 		this.basePath = basePath;
 		this.secure = secure;
+		this.axiosClient = client;
 	}
-	// TODO: this request probably shouldn't be serialisable, that interface should be for websockets
-	public async SendUnary<ResT = any>(endpoint: string, request: Serialisable, parseResponse: ((res: Object) => ResT) | ((res: Object) => Promise<ResT>)): Promise<ResT> {
-		let res = await axios.post<Object>(this.BuildURL(endpoint), request.Serialise());
+	/**
+	 * @param {string} endpoint The API endpoint to send the message to
+	 * @param {ProtoJSONCompatible} request The message to convert to protojson then send
+	 */
+	public async SendUnary<ReqT extends ProtoJSONCompatible, ResT = any>(endpoint: string, request: ReqT, parseResponse: Parser<ResT>): Promise<ResT> {
+		let message = request.ToProtoJSON();
+		let url = this.BuildURL(endpoint);
+		let res = await this.axiosClient.post<Object>(url, message);
 		return parseResponse(res.data);
+	}
+	public async StartClientStream<ReqT extends ProtoJSONCompatible, ResT = any>(endpoint: string, parseResponse: Parser<ResT>): Promise<EstablishedWebsocket<ReqT, ResT>> {
+		let url = this.BuildURL(endpoint);
+		let ws = new HTTPgRPCWebSocket(url, parseResponse);
+		// Establish the connection, set up event listeners, etc.
+		return ws.init();
 	}
 	public BuildURL(endpoint: string, websocket: boolean = false): string {
 		// First get the scheme
@@ -41,8 +57,20 @@ export class Client {
 	}
 }
 
-export interface Serialisable {
-	Serialise(): Object
+/** Many messages are simpler to build and manage using native types that aren't 100% identical to what is expected by the canonical JSON representation of those messages.
+ * 
+ * To deal with this, you are encouraged to use classes that hold those native values, but implement a "Serialise" function that converts them to the protojson format.
+ * 
+ * For messages sent on unary RPCs, these objects will be sent as-is on the provided axios client in HTTP requests.
+ * 
+ * For messages sent on streamed RPCs, these objects will be passed through JSON.stringify and sent on the websocket send channel.
+ */
+export interface ProtoJSONCompatible {
+	/** Convert native fields to canonical protojson format
+	 * 
+	 * e.g. 64-bit numbers as strings, bytes as base64, oneofs as specific instance fields
+	 * */
+	ToProtoJSON(): Object
 }
 
 export default Client;
---
+++ client/src/websocket/websocket.ts
@@ -0,0 +1,67 @@
+import { Parser, ProtoJSONCompatible } from "client";
+
+const EOFMessage = "EOF";
+
+export class HTTPgRPCWebSocket<ReqT extends ProtoJSONCompatible, ResT = any> {
+	private parser: Parser<ResT>;
+	private url: string;
+	private initialised: boolean = false;
+	private conn?: WebSocket = undefined;
+	private responseBuffer: string[] = [];
+	constructor(url: string, parser: Parser<ResT>) {
+		this.parser = parser;
+		this.url = url;
+	}
+	/** Attempts to establish a websocket connection, then set up event listeners to handle bi-directional comms.
+	 * 
+	 * Can only be called once, will throw an error if it is called a second time.
+	 * 
+	 * This will normally be called by the Client method which establishes the connection, you only need to use this method
+	 * if you are creating instances of this class directly for some reason.
+	 * 
+	 * Calling this a second time will always throw an error without doing anything else.
+	 */
+	public async init(): Promise<EstablishedWebsocket<ReqT, ResT>> {
+		if (this.initialised) {
+			throw new Error("cannot initialise HTTPgRPCWebSocket twice");
+		}
+		this.conn = new WebSocket(this.url);
+		// FIXME: event listeners
+		return this;
+	}
+	public async Send(request: ReqT): Promise<void> {
+		let message = request.ToProtoJSON();
+		let messageString = JSON.stringify(message);
+		this.conn?.send(messageString);
+	}
+	public async Recv(): Promise<ResT> {
+		let next = this.responseBuffer.shift();
+		if (next === undefined) {
+			// FIXME this isn't how buffers work, redo this whole section
+			throw new Error("unimplemented")
+		}
+		return this.parser(next);
+	}
+	public async CloseSend(): Promise<void> {
+		this.conn?.send(EOFMessage);
+	}
+	public async Close(code?: number, reason?: string): Promise<void> {
+		this.conn?.close(code, reason);
+	}
+}
+
+/** A websocket connection open and ready to handle gRPC message transfer in both directions.
+ * 
+ * Wrap this in one of the specialised gRPC streaming patterns if you wish to simplify the 
+ * API and not have to worry about when to call CloseSend, etc.
+ */
+export interface EstablishedWebsocket<ReqT extends ProtoJSONCompatible, ResT = any> {
+	/** Wait until a message is successfully sent to the server. */
+	Send(request: ReqT): Promise<void>;
+	/** Wait until a message is received from the server. */
+	Recv(): Promise<ResT>;
+	/** Close the sending direction of communications, any Send calls after this will throw an Error without writing to the websocket. */
+	CloseSend(): Promise<void>;
+	/** Close terminates the websocket connection early, resulting in errors at the server's end. Any other calls after this point will throw an Error. */
+	Close(code?: number, reason?: string): Promise<void>;
+}
