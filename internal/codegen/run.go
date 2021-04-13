7aaf4eca5a1d4b1e7d085a630c5f1c593c774c58
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-04-14 09:17:26 +1000

    Fix relative imports

--- internal/codegen/run.go
+++ internal/codegen/run.go
@@ -216,6 +216,14 @@ func generateImports(f *descriptorpb.FileDescriptorProto, content *strings.Build
 	if useGoogle {
 		content.WriteString("import { google } from \"@llkennedy/protoc-gen-tsjson\";\n")
 	}
+	prefix := "./"
+	currentParts := strings.Split(f.GetName(), "/")
+	if len(currentParts) > 1 {
+		prefix = ""
+	}
+	for i := 1; i < len(currentParts); i++ {
+		prefix += "../"
+	}
 	for importPath, imports := range importMap {
 		fullImportList := &strings.Builder{}
 		for i, imp := range imports {
@@ -225,7 +233,7 @@ func generateImports(f *descriptorpb.FileDescriptorProto, content *strings.Build
 			fullImportList.WriteString("\n	")
 			fullImportList.WriteString(imp)
 		}
-		content.WriteString(fmt.Sprintf("import { %s\n} from \"%s\";\n", fullImportList.String(), importPath))
+		content.WriteString(fmt.Sprintf("import { %s\n} from \"%s%s\";\n", fullImportList.String(), prefix, importPath))
 	}
 	content.WriteString("\n")
 }
